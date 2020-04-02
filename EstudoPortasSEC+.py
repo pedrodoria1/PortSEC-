f25a81f630d7272fa06e1b718b5a2a3213e27ac5
Pedro Doria <pedro.doria@outlook.com.br>
2020-04-02 14:07:30 -0300

    Update EstudoPortasSEC+.py
    Adicionando Score e mais alguns protocolos.

--- EstudoPortasSEC+.py
+++ EstudoPortasSEC+.py
@@ -1,19 +1,34 @@
 from random import choice
 
-protocols = ['ftp','ssh','telnet','snmp']
-ports = [21,22,23,161]
-ftp = [protocols[0]] + [str(ports[0])]
-ssh = [protocols[1]] + [str(ports[1])]
-telnet = [protocols[2]] + [str(ports[2])]
-snmp = [protocols[3]] + [str(ports[3])]
-consolidated = [ftp,ssh,telnet,snmp]
-randomPort = choice(consolidated)
-print('Qual o número da porta equivalente ao protocolo: {}'.format(randomPort[0]))
-answer = int(input('Digite: '))
-if str(answer) == randomPort[1]:
-    print('RIGHT!')
-else:
-    print('WRONG!')
-print('Resposta: Protocolo {} | Porta {}'.format(randomPort[0],randomPort[1]))
+score = 0
+
+def question():
+    global score
+    protocols = ['ftp','ssh','telnet','snmp','rdp','https','http','smtp']
+    ports = [21,22,23,161,3389,443,8080,25]
+    ftp = [protocols[0]] + [str(ports[0])]
+    ssh = [protocols[1]] + [str(ports[1])]
+    telnet = [protocols[2]] + [str(ports[2])]
+    snmp = [protocols[3]] + [str(ports[3])]
+    rdp = [protocols[4]] + [str(ports[4])]
+    https = [protocols[5]] + [str(ports[5])]
+    http = [protocols[6]] + [str(ports[6])]
+    smtp = [protocols[7]] + [str(ports[7])]
+    consolidated = [ftp,ssh,telnet,snmp,rdp,https,http,smtp]
+    randomPort = choice(consolidated)
+    print('Qual o número da porta equivalente ao protocolo: {}'.format(randomPort[0]))
+    answer = int(input('Digite: '))
+    if str(answer) == randomPort[1]:
+        print('RIGHT!')
+        score = score + 1
+    else:
+        print('WRONG!')
+        score = score - 1
+    print('Resposta: Protocolo {} | Porta {}'.format(randomPort[0],randomPort[1]))
+    print(score)
+    question()
+
+
+question()
 
 
